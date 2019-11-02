9b5d5c177bb7205b39c6f30d09fe031d81e0fb55
toonnyy8 <toonnyy8@gmail.com>
2019-11-02 20:38:53 +0800

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
+import Vue from "vue"
+
+import index from "../vue/index.vue"
+
+new Vue(index).$mount('#main')
---
+++ src/vue/index.vue
@@ -0,0 +1,85 @@
+<template>
+	<div>
+		<div class="b">專題組別</div>
+	</div>
+</template>
+
+<script defer>
+export default {
+	data() {
+		document.documentElement.style.setProperty(
+			"--screen-height",
+			document.body.offsetHeight + "px"
+		);
+		document.documentElement.style.setProperty(
+			"--screen-width",
+			document.body.offsetWidth + "px"
+		);
+		window.onresize = () => {
+			document.documentElement.style.setProperty(
+				"--screen-height",
+				document.body.offsetHeight + "px"
+			);
+			document.documentElement.style.setProperty(
+				"--screen-width",
+				document.body.offsetWidth + "px"
+			);
+		};
+		return {};
+	}
+};
+</script>
+
+<style>
+@font-face {
+	font-family: custom-serif;
+	src: local(Microsoft JhengHei), local("DFKai-sb");
+	unicode-range: U+4E00-9FFF;
+}
+
+@font-face {
+	font-family: custom-serif;
+	src: local(Source Code Pro), local(Arial);
+	unicode-range: U+00-024F;
+}
+
+html {
+	height: 100%;
+	font-family: custom-serif;
+}
+
+body {
+	height: 100%;
+	margin: 0px;
+	display: flex;
+	flex-direction: column;
+}
+:root {
+	--screen-height: 100px;
+	--screen-width: 100px;
+}
+</style>
+        
+<style scoped>
+.b {
+	text-align: center;
+	flex: 1;
+	margin-left: auto;
+	background-color: rgb(66, 66, 66);
+	color: white;
+	transition-property: width, line-height;
+	transition-duration: 1s;
+	width: calc(5% + 100px);
+	height: var(--screen-height);
+	line-height: calc(var(--screen-width) * 0.05 + 100px);
+	-webkit-writing-mode: vertical-lr;
+	writing-mode: vertical-lr;
+}
+
+.b:hover {
+	transition-property: width, line-height;
+	transition-duration: 1s;
+	line-height: calc(var(--screen-width) * 0.1 + 200px);
+	width: calc(10% + 200px);
+}
+</style>
