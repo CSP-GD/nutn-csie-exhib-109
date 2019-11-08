94a0eb76328016481acb0135ab8fe84572b9dfde
lian0123 <lienyungli123@gmail.com>
2019-11-09 00:16:17 +0800

    新增小組資料結構

---
+++ src/js/studentgroup.js
@@ -0,0 +1,342 @@
+var StudentGroupJson = [
+    /* 第一組 */
+    {
+        GroupID      :  1, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "知識本體應用暨軟體工程實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10459044", StudentName:"洪誠安" }
+        ]
+    },
+    /* 第二組 */
+    {
+        GroupID      :  2, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "智慧型機器人實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559001", StudentName:"徐偉倫" },
+            { StudentID:"S10559044", StudentName:"洪于婕" }
+        ]
+    },
+    /* 第三組 */
+    {
+        GroupID      :  3, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "多媒體網路實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559002", StudentName:"蘇政穎" },
+            { StudentID:"S10559004", StudentName:"陳銘濡" }
+        ]
+    },
+    /* 第四組 */
+    {
+        GroupID      :  4, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "資料系統實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559003", StudentName:"林迺航" },
+            { StudentID:"S10559022", StudentName:"廖毅軒" }
+        ]
+    },
+    /* 第五組 */
+    {
+        GroupID      :  5, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "資料庫與資訊知識系統實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559005", StudentName:"高嘉謙" },
+            { StudentID:"S10559006", StudentName:"郭峻霖" }
+        ]
+    },
+    /* 第六組 */
+    {
+        GroupID      :  6, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "多媒體網路實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559007", StudentName:"丁吾心" },
+            { StudentID:"S10559026", StudentName:"羅宇宸" }
+        ]
+    },
+    /* 第七組 */
+    {
+        GroupID      :  7, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "資料系統實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559008", StudentName:"游士玄" }
+        ]
+    },
+    /* 第八組 */
+    {
+        GroupID      :  8, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "知識本體應用暨軟體工程實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559009", StudentName:"李原鋌" },
+            { StudentID:"S10559024", StudentName:"李誌軒" },
+            { StudentID:"S10559030", StudentName:"黃勝揮" }
+        ]
+    },
+    /* 第九組 */
+    {
+        GroupID      :  9, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "行動計算與網路實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559010", StudentName:"郭憲儒" },
+            { StudentID:"S10559021", StudentName:"吳柏賢" }
+        ]
+    },
+    /* 第十組 */
+    {
+        GroupID      : 10, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "電腦輔助系統設計實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559012", StudentName:"劉鎮維" },
+            { StudentID:"S10559045", StudentName:"胡君愷" }
+        ]
+    },
+    /* 第十一組 */
+    {
+        GroupID      : 11, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "多媒體與醫學影像實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559013", StudentName:"佘宜道" }
+        ]
+    },
+    /* 第十二組 */
+    {
+        GroupID      : 12, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "電腦輔助系統設計實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559014", StudentName:"廖御閎" },
+            { StudentID:"S10559027", StudentName:"陳巍中" }
+        ]
+    },
+    /* 第十三組 */
+    {
+        GroupID      : 13, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "行動計算與網路實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559015", StudentName:"周欣玄" },
+            { StudentID:"S10559018", StudentName:"葉瓊斯" }
+        ]
+    },
+    /* 第十四組 */
+    {
+        GroupID      : 14, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "知識本體應用暨軟體工程實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559016", StudentName:"張維珊" },
+            { StudentID:"S10559036", StudentName:"周晨瑋" }
+        ]
+    },
+    /* 第十五組 */
+    {
+        GroupID      : 15, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "多媒體與醫學影像實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559017", StudentName:"趙云珮" }
+        ]
+    },
+    /* 第十六組 */
+    {
+        GroupID      : 16, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "多媒體與醫學影像實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559019", StudentName:"林萱" },
+            { StudentID:"S10559031", StudentName:"蔡亞倫" }
+        ]
+    },
+    /* 第十七組 */
+    {
+        GroupID      : 17, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "電腦輔助系統設計實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559020", StudentName:"連永立" }
+        ]
+    },
+    /* 第十八組 */
+    {
+        GroupID      : 18, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "資料庫與資訊知識系統實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559023", StudentName:"鄭聖玄" },
+            { StudentID:"S10559043", StudentName:"金鑒修" }
+        ]
+    },
+    /* 第十九組 */
+    {
+        GroupID      : 19, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "多媒體網路實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559025", StudentName:"黃祈緯" },
+        ]
+    },
+    /* 第二十組 */
+    {
+        GroupID      : 20, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "生物資訊實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559028", StudentName:"黃仁鴻" },
+        ]
+    },
+    /* 第二十一組 */
+    {
+        GroupID      : 21, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "電腦輔助系統設計實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559032", StudentName:"吳孟霖" }
+        ]
+    },
+    /* 第二十二組 */
+    {
+        GroupID      : 22, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "多媒體與醫學影像實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559034", StudentName:"楊明諺" }
+        ]
+    },
+    /* 第二十三組 */
+    {
+        GroupID      : 23, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "資料系統實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559035", StudentName:"楊政碩" },
+            { StudentID:"S10559039", StudentName:"隋昊璟" }
+        ]
+    },
+    /* 第二十四組 */
+    {
+        GroupID      : 24, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "智慧型機器人實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559037", StudentName:"張俞方" },
+            { StudentID:"S10559040", StudentName:"施宣妤" }
+        ]
+    },
+    /* 第二十五組 */
+    {
+        GroupID      : 25, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "行動計算與網路實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559041", StudentName:"呂玉嫻" }
+        ]
+    },
+    /* 第二十六組 */
+    {
+        GroupID      : 26, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "智慧型機器人實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559042", StudentName:"于鶴瑤" }
+        ]
+    },
+    /* 第二十七組 */
+    {
+        GroupID      : 27, //專題編號
+        ProjectName  : "", //專題名稱
+        ImgSrc       : "", //專題截圖
+        VideoSrc     : "", //專題影片
+        FileSrc      : "", //檔案連結
+        FromLab      : "多媒體與醫學影像實驗室" , //所屬實驗室
+        GroupStudent : [
+            { StudentID:"S10559046", StudentName:"顏婷玉" }
+        ]
+    },
+];
