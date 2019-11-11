84858e9300e1213009c83355219010d085ddc90a
toonnyy8 <toonnyy8@gmail.com>
2019-11-11 16:04:55 +0800

    移除多餘程式

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,6 @@ import "core-js/stable"
 import "regenerator-runtime/runtime"
 import Vue from "vue"
 
-import home from "../vue/home.vue"
-import info from "../vue/info.vue";
-import gradient from "../vue/gradient.vue";
 import main from "../vue/main.vue"
 
 new Vue(main).$mount('#main')
